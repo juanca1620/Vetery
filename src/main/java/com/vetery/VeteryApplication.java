1f3829a247cc7fba704fff4ac5ac8bd3042a0cce
= <=>
2025-04-03 15:09:17 -0500

    beging commit

---
+++ src/main/java/com/vetery/VeteryApplication.java
@@ -0,0 +1,13 @@
+package com.vetery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeteryApplication.class, args);
+	}
+
+}
